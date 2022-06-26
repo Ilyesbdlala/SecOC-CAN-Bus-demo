2f041671d1dcfa4c80746ec2be9f9a59920b1f93
Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>
2022-06-26 14:03:01 +0200

    Create corrupt_send.py

---
+++ corrupt_send.py
@@ -0,0 +1,19 @@
+import os
+import can
+import sys
+
+os.system('sudo ip link set can0 type can bitrate 100000')
+os.system('sudo ifconfig can0 up')
+
+can0 = can.interface.Bus(channel = 'can0',bustype = 'socketcan_ctypes')# soc>
+
+msg = can.Message(arbitration_id=ord('0'),data=bytearray(b'A\x40goode0'),ext>
+
+can0.send(msg)
+f = open('./msgCounter')
+file_read = int(f.read())
+
+f = open('./msgCounter','w')
+f.write(str(file_read+1))
+
+os.system('sudo ifconfig can0 down')
